d407e2dd8ff5cf6e056ce3a026a45f0e5c385d9f
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-29 16:09:13 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Beyza Nur Tekerek on 29.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
