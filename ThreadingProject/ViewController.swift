a0e7830895dc643f98550d1310f95075fb5ea4b8
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-29 23:57:39 +0300

    first commit

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -7,11 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let urlStrings = ["https://images.pexels.com/photos/3774243/pexels-photo-3774243.jpeg?cs=srgb&dl=pexels-souvenirpixels-3774243.jpg&fm=jpg", "https://plus.unsplash.com/premium_photo-1661871178548-e35aece53d53?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGFyZ2V8ZW58MHx8MHx8fDA%3D"]
+    
+    var data = Data()
+    var tracker = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        DispatchQueue.global().async {
+            self.data = try! Data(contentsOf: URL(string: self.urlStrings[self.tracker])!) // background thread
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: self.data) // main thread
+            }
+        }
+
+        
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(changeImage))
+        
+    }
+    
+    @objc func changeImage() {
+        
+        if tracker == 0 {
+            tracker += 1
+        } else {
+            tracker -= 1
+        }
+        
+        DispatchQueue.global().async {
+            self.data = try! Data(contentsOf: URL(string: self.urlStrings[self.tracker])!)
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: self.data)
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Threading test"
+        return cell
     }
 
 
